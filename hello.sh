add0140b3cec2ef4b06bbfe561f442a975024879
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-05 16:21:50 +0000

    added help function to test script

--- hello.sh
+++ hello.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+# This will create a help function to display options for running the script.
+
+help() 
+{
+	# Display Help message
+	echo "Add whatever description of the script functions on this line."
+	echo 
+	echo "Syntax: <script_name> [-g|h|v|V]"
+	echo "options:"
+	echo "g    Run general script."
+	echo "h    Print this Help message."
+	echo "v    Run verbose mode on script"
+	echo "V    Print the version of script"
+}
+
+help
+
 echo "Hello World!"
