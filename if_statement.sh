6c478efe7d084bdde056f59291573401b8a72d87
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-26 23:22:12 -0400

    added to echo statement

--- if_statement.sh
+++ if_statement.sh
@@ -2,5 +2,5 @@
 if [[ $(hostname -s) == "cloudhost-"* ]]; then
     echo "You are on a cloud server."
 else
-    echo "You are on a physical server."
+    echo "You are on a physical server or somewhere else."
 fi
