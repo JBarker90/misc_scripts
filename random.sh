7dcb0f4d41abb1e68b04e15e7fabc619ae8d59b6
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-27 15:27:59 +0000

    fixed if statement argument

--- random.sh
+++ random.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-if [[ -d "/home/jonathan/test_dir/sample.txt" ]]; then
+if [[ -e "/home/jonathan/test_dir/sample.txt" ]]; then
     cat -n /home/jonathan/test_dir/sample.txt
-else
-    echo "This file path does NOT exist. You may need to create one or check your file path."
 fi
