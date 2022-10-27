62b7327730eb6ba2a965a4c9ca97a7cac6961453
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-27 15:31:41 +0000

    added else statement to my test

--- random.sh
+++ random.sh
@@ -2,4 +2,6 @@
 
 if [[ -e "/home/jonathan/test_dir/sample.txt" ]]; then
     cat -n /home/jonathan/test_dir/sample.txt
+else
+    echo "This file path does NOT exist. You may need to create one or check your file path."
 fi
