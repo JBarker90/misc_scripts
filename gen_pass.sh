159e0e4318cabb51d6c6e6f61a5d989752901f7d
Jonathan Barker <jonathan.barker09@gmail.com>
2023-02-01 17:20:54 +0000

    created new shell script to generate a password

---
+++ gen_pass.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script generates a random password.
+# This user can set the password length with -l and add a special character with -s.
+# Verbose mode can be enabled with -v
+
+# Set a default password length
+LENGTH=48 
+
+while getopts 'vl:s' OPTION; do
+    case ${OPTION} in
+        v)
+          VERBOSE='true'
+          echo 'Verbose mode on.'
+          ;;
+        l)
+          LENGTH="${OPTARG}"
+          ;;
+        s)
+          USE_SPECIAL_CHARACTER='true'
+          ;;
+        ?)
+          echo 'Invalid option.' >&2
+          exit 1
+          ;;
+    esac
+done
